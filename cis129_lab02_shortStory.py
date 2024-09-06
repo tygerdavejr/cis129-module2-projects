9ee2fa0f81c9d5e7abbc5d0d4fc5ef40a08d998b
tygerdavejr <dvance4@mail.pima.edu>
2024-09-05 23:34:50 -0700

    cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,8 +1,1 @@
-# Author: David Vance
-# My first Python lab for CIS 129
-# This prints a simple story and demonstrates a few different print function characteristics
-print('David Vance')
-print('CIS 129: Programming and \
-Problem Solving')
-print("Professor Kevin Chang")
-print("""I might not be as 'lost in the woods' as I feared""")
+print('Hello, World!')
